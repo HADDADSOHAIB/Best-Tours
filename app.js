1358859c2da7f0b195c487d73542d41c7b414e0a
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-01-17 23:05:38 +0100

    integrate security xss sanitizing

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express=require('express');
 const rateLimit=require('express-rate-limit');
+const helmet=require('helmet');
 const app=express();
 const morgan=require('morgan');
 const AppError=require('./utils/appError');
 const globalErrorHandler=require('./controllers/errorController');
+const mongoSanitize=require('express-mongo-sanitize');
+const xss=require('xss-clean');
+
 if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'));
 }
-app.use(express.json());
+app.use(helmet());
+app.use(express.json({ limit:'10kb'}));
 app.use(express.static(`${__dirname}/public`));
+app.use(mongoSanitize());
+app.use(xss());
 
 const limiter=rateLimit({
     max:100,
