62ca0817c5ab77143481208b311dd7557d10e15f
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-01-20 11:23:27 +0100

    refactoring

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,6 @@ const router=express.Router();
 const userController=require('../controllers/userController');
 const authController=require('../controllers/authController');
 
-
 router.post('/signup',authController.signup);
 router.post('/login',authController.login);
 router.post('/forgotPassword',authController.forgotPassword);
