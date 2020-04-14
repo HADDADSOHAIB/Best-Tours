3d00673df741eeced0e80b87b92c0647ce0cd2e8
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-14 01:35:23 +0100

    add submit logic

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,5 @@
 
+
 const username = document.querySelector('#username');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
@@ -37,14 +38,14 @@ const validateEmail = e => {
     email.className = 'invalid form-control';
     let notice_required_element = email.parentElement.querySelector('.notice-required');
     if(!notice_required_element && !is_exit){
-      let notice = `<small class="form-text notice-required">*The email is a required field.</small>`;
+      let notice = `<small class="form-text notice notice-required">*The email is a required field.</small>`;
       email.insertAdjacentHTML('afterend',notice);
       notice_required_element = email.parentElement.querySelector('.notice-required');
       notice_required_element.style.color = 'red';
     }
     let notice_valid_email_element = email.parentElement.querySelector('.notice-valid-email');
     if(!notice_valid_email_element && !is_valid_email){
-      let notice = `<small class="form-text notice-valid-email">*The email is not valid.</small>`;
+      let notice = `<small class="form-text notice notice-valid-email">*The email is not valid.</small>`;
       email.insertAdjacentHTML('afterend',notice);
       notice_valid_email_element = email.parentElement.querySelector('.notice-valid-email');
       notice_valid_email_element.style.color = 'red';
@@ -72,14 +73,14 @@ const validatePassword = e => {
     password.className = 'invalid form-control';
     let notice_required_element = password.parentElement.querySelector('.notice-required');
     if(!notice_required_element && !is_exit){
-      let notice = `<small class="form-text notice-required">*The password is a required field.</small>`;
+      let notice = `<small class="form-text notice notice-required">*The password is a required field.</small>`;
       password.insertAdjacentHTML('afterend',notice);
       notice_required_element = password.parentElement.querySelector('.notice-required');
       notice_required_element.style.color = 'red';
     }
     let notice_long_element = password.parentElement.querySelector('.notice-long');
     if(!notice_long_element && !is_long){
-      let notice = `<small class="form-text notice-long">*The password length should be more then 8.</small>`;
+      let notice = `<small class="form-text notice notice-long">*The password length should be more then 8.</small>`;
       password.insertAdjacentHTML('afterend',notice);
       notice_long_element = password.parentElement.querySelector('.notice-long');
       notice_long_element.style.color = 'red';
@@ -93,7 +94,52 @@ const validatePassword = e => {
     let notice_long_element = password.parentElement.querySelector('.notice-long');
     if(notice_long_element) notice_long_element.parentElement.removeChild(notice_long_element);
   }
+
+  validatePasswordConfirmation();
 }
 
 password.addEventListener('keyup', validatePassword);
-P
+
+const validatePasswordConfirmation = (e = null) => {
+  let value = (e ? e.target.value : password_confirmation.value);
+  let is_valid = value === password.value;
+
+  if(!is_valid) {
+    password_confirmation.className = 'invalid form-control';
+    let notice_element = password_confirmation.parentElement.querySelector('.notice');
+    if(!notice_element){
+      let notice = `<small class="form-text notice">*the password confirmation is not the same.</small>`;
+      password_confirmation.insertAdjacentHTML('afterend',notice);
+      notice_element = password_confirmation.parentElement.querySelector('.notice');
+      notice_element.style.color = 'red';
+    }
+  }
+  else{
+    password_confirmation.className = 'form-control';
+    let notice_element = password_confirmation.parentElement.querySelector('.notice');
+    if(notice_element) notice_element.parentElement.removeChild(notice_element);
+  }
+}
+
+password_confirmation.addEventListener('keyup', validatePasswordConfirmation)
+
+const submit = (e) =>{
+  e.preventDefault();
+  let valid = email.value && username.value && password && password_confirmation && !document.querySelector('.notice');
+  if(valid){
+    axios.post(`${window.location.host}/api/v1/users/signup`, {
+      name: username.value,
+      email: email.value,
+      password: password.value,
+      confirmPassword: password_confirmation.value
+    })
+    .then( response => {
+      console.log(response);
+    })
+    .catch(ferror => {
+      console.log(error);
+    });
+  }
+}
+
+document.querySelector('button[type="submit"]').addEventListener('click', submit);
