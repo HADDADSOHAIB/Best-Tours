6a9e8a7c9d5cde178d19aea6ad8d2c9dc3dbbced
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-22 00:01:53 +0000

    fix booking send email bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const Tour=require('./../models/tourModel');
 const Booking=require('./../models/bookingModel');
+const User = require('../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const Email=require('./../utils/email');
 const stripe = require('stripe')('sk_test_hE2iOItUacnaA3fEOI3ip9zp00gjGoK4w0');
@@ -9,7 +10,7 @@ exports.getCheckoutSession = catchAsync( async(req, res, next) => {
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&&user=${JSON.stringify(req.user)}&&price=${tour.price}&&tourName=${tour.name}`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&&user=${req.user._id}&&price=${tour.price}&&tourName=${tour.name}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -31,21 +32,25 @@ exports.getCheckoutSession = catchAsync( async(req, res, next) => {
 });
 
 exports.createBookingCheckout  = (req, res, next) => {
+
   const { tour, user, price, tourName } = req.query;
   if(!tour || !user || !price){
     next();
   }
   else{
-    let email = new Email(JSON.parse(user),`${req.protocol}://${req.get('host')}/my-tours`,{ tourName });
-    Booking.create({tour, user: JSON.parse(user)._id , price }, function (err, response) {
-      if (err){
-        console.log(err);
-        email.sendTourNotBooked();
-      }
-      else {
-        email.sendTourBooked();
-      }
+    User.findById(user, (err, userDoc) => {
+      let email = new Email(userDoc,`${req.protocol}://${req.get('host')}/my-tours`,{ tourName });
+      Booking.create({tour, user, price }, function (err, response) {
+        if (err){
+          console.log(err);
+          email.sendTourNotBooked();
+        }
+        else {
+          email.sendTourBooked();
+        }
+      });
     });
+    
     res.redirect(301,`${req.protocol}://${req.get('host')}/tour-booked/${tour}`);
   }
 };
