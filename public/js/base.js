e608188b4dbe0edff48e2b34eebb3043e1a4f1c9
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-23 22:33:59 +0000

    fix cookies bugs

--- public/js/base.js
+++ public/js/base.js
@@ -1,5 +1,6 @@
 const signout = () => {
-  document.cookie = "token_user= ; expires = expires=Thu, 01 Jan 1970 00:00:01 GMT"
+  console.log("signout");
+  document.cookie = "token_user=; expires = -1; path=/"
   window.location.assign('/');
 }
 
